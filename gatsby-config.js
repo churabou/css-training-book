348ee69bbe512dcda665f83560a12d57fb5cc38f
churabou <ginga7888@gmail.com>
2020-05-21 20:58:09 +0900

    Move pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: `${__dirname}/contents/`,
       },
     },
-    `gatsby-transformer-remark`
-  ]
-}
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-styled-components`,
+  ],
+};
